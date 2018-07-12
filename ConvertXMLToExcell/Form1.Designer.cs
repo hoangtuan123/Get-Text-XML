54959ebbebf39ff811c3079a30744e95a5af212d
Tuan Nguyen <tuan.nguyen@exe.com.vn>
2018-07-12 17:05:59 +0700

    init project

---
+++ ConvertXMLToExcell/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ConvertXMLToExcell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdExcelToXML = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExportFolder = new System.Windows.Forms.TextBox();
+            this.btnChoiceFolderExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnChoiceImportFolder = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdXmlToExcel = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdExcelToXML
+            // 
+            this.rdExcelToXML.AutoSize = true;
+            this.rdExcelToXML.Location = new System.Drawing.Point(186, 29);
+            this.rdExcelToXML.Name = "rdExcelToXML";
+            this.rdExcelToXML.Size = new System.Drawing.Size(88, 17);
+            this.rdExcelToXML.TabIndex = 10;
+            this.rdExcelToXML.TabStop = true;
+            this.rdExcelToXML.Text = "Excel to XML";
+            this.rdExcelToXML.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Export Folder:";
+            // 
+            // txtExportFolder
+            // 
+            this.txtExportFolder.Location = new System.Drawing.Point(86, 66);
+            this.txtExportFolder.Name = "txtExportFolder";
+            this.txtExportFolder.ReadOnly = true;
+            this.txtExportFolder.Size = new System.Drawing.Size(297, 20);
+            this.txtExportFolder.TabIndex = 13;
+            // 
+            // btnChoiceFolderExport
+            // 
+            this.btnChoiceFolderExport.Location = new System.Drawing.Point(389, 64);
+            this.btnChoiceFolderExport.Name = "btnChoiceFolderExport";
+            this.btnChoiceFolderExport.Size = new System.Drawing.Size(75, 23);
+            this.btnChoiceFolderExport.TabIndex = 12;
+            this.btnChoiceFolderExport.Text = "...";
+            this.btnChoiceFolderExport.UseVisualStyleBackColor = true;
+            this.btnChoiceFolderExport.Click += new System.EventHandler(this.btnChoiceFolderExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Import Folder:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(86, 30);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(297, 20);
+            this.txtFolder.TabIndex = 10;
+            // 
+            // btnChoiceImportFolder
+            // 
+            this.btnChoiceImportFolder.Location = new System.Drawing.Point(389, 28);
+            this.btnChoiceImportFolder.Name = "btnChoiceImportFolder";
+            this.btnChoiceImportFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnChoiceImportFolder.TabIndex = 9;
+            this.btnChoiceImportFolder.Text = "...";
+            this.btnChoiceImportFolder.UseVisualStyleBackColor = true;
+            this.btnChoiceImportFolder.Click += new System.EventHandler(this.btnChoiceImportFolder_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 68);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(452, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(389, 26);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 16;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnChoiceImportFolder);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnChoiceFolderExport);
+            this.groupBox1.Controls.Add(this.txtExportFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 109);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Folder";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnConvert);
+            this.groupBox2.Controls.Add(this.rdXmlToExcel);
+            this.groupBox2.Controls.Add(this.rdExcelToXML);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(487, 100);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Action";
+            // 
+            // rdXmlToExcel
+            // 
+            this.rdXmlToExcel.AutoSize = true;
+            this.rdXmlToExcel.Location = new System.Drawing.Point(12, 29);
+            this.rdXmlToExcel.Name = "rdXmlToExcel";
+            this.rdXmlToExcel.Size = new System.Drawing.Size(92, 17);
+            this.rdXmlToExcel.TabIndex = 9;
+            this.rdXmlToExcel.TabStop = true;
+            this.rdXmlToExcel.Text = "XML To Excel";
+            this.rdXmlToExcel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 272);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton rdExcelToXML;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExportFolder;
+        private System.Windows.Forms.Button btnChoiceFolderExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnChoiceImportFolder;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdXmlToExcel;
+    }
+}
+
---
+++ ConvertXMLToExcell/Form1.cs
@@ -0,0 +1,315 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ConvertXMLToExcell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            rdExcelToXML.Checked = true;
+        }
+
+        private List<ExportFile> getDataFromFolder(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path, "*.xlsx", SearchOption.TopDirectoryOnly);
+            var listAll = new List<ExportFile>();
+            foreach (var filePath in filePaths)
+            {
+                var list = readDataFromExcel(filePath);
+                if (list != null && list.Count > 0)
+                    listAll.AddRange(list);
+            }
+            return listAll;
+        }
+
+        private bool updateFile(string path, List<ExportFile> words)
+        {
+            string text = File.ReadAllText(path);
+            var temp = words.OrderByDescending(n => n.Length);
+            foreach(var item in temp)
+            {
+                if(!string.IsNullOrWhiteSpace(item.English) && !string.IsNullOrWhiteSpace(item.NewLang))
+                    text = text.Replace(item.English, item.NewLang);
+            }
+            
+            File.WriteAllText(path, text);
+
+            return true;
+        }
+
+
+        private List<ExportFile> readDataFromExcel(string path)
+        {
+            var list = new List<ExportFile>();
+            var fileInfo = new FileInfo(path);
+
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var workbook = package.Workbook;
+                var worksheet = workbook.Worksheets.First();
+
+                for (int i = worksheet.Dimension.Start.Row + 1; i <= worksheet.Dimension.End.Row; i++)
+                {
+                    string File = worksheet.Cells[i, 1].Value.ToString();
+                    string English = worksheet.Cells[i, 2].Value.ToString();
+                    string NewLang = worksheet.Cells[i, 3].Value.ToString();
+                    var newLinesRegex = new Regex(@"\r\n|\n|\r", RegexOptions.Singleline);
+                    List<string> listStr = newLinesRegex.Split(File).ToList();
+                    foreach (var str in listStr)
+                    {
+                        if(!string.IsNullOrWhiteSpace(str))
+                            list.Add(new ExportFile { File = str, English = English, NewLang = NewLang, Length = English.Length });
+                    }
+                    
+                }
+            }
+
+            return list;
+        }
+
+
+        private async Task<bool> convertExcellToXML()
+        {
+            string pathExcel = txtFolder.Text;
+
+            var listData = getDataFromFolder(pathExcel);
+
+            var listFile = listData.GroupBy(n => n.File, (key, g) => new { File = key, Words = g.ToList() });
+            foreach(var item in listFile)
+            {
+                string path = txtExportFolder.Text + item.File;
+                updateFile(path, item.Words);
+            }
+
+            progressBar1.Value = 100;
+            return true;
+        }
+
+        private async Task<bool> convertXMLToExcel()
+        {
+            string path = txtFolder.Text;
+
+            DirectoryInfo directionInfo = new DirectoryInfo(path);
+            string[] filePaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            List<ExportFile> listFile = new List<ExportFile>();
+
+            IProgress<int> progress = new Progress<int>(v =>
+            {
+                // This lambda is executed in context of UI thread,
+                // so it can safely update form controls
+                Console.WriteLine(v);
+                progressBar1.Value = v;
+            });
+
+            await Task.Run(() =>
+            {
+                progress.Report(50);
+                try
+                {
+                    int i = 0;
+                    foreach (var filePath in filePaths)
+                    {
+                        List<string> listStr = new List<string>();
+                        string extension = GetPathFile(filePath);
+                        if (extension == ".xml")
+                        {
+                            listStr = GetStringFromXML(filePath);
+                        }
+                        else if (extension == ".asp")
+                        {
+                            listStr = GetStringFromASP(filePath);
+                        }
+
+                        string tempPath = filePath.Replace(path, "");
+                        foreach (var str in listStr)
+                        {
+                            listFile.Add(new ExportFile { File = tempPath, English = str });
+                        }
+
+                        if (i != 0 && filePaths.Length != 0)
+                        {
+                            double per = (double)i / (double)filePaths.Length * (double)100;
+                            int percent = (int)Math.Floor(per);
+                            if (percent > 0)
+                            {
+                                progress.Report(percent);
+                            }
+                        }
+
+                        i++;
+                    }
+
+                    var list = listFile.GroupBy(n => n.English
+                    , (key, g) => new
+                    {
+                        English = key,
+                        File = g.Aggregate(string.Empty, (item, next) => item + next.File + Environment.NewLine)
+                    });
+
+
+                    DataTable dt = new DataTable();
+
+                    dt.Columns.Add("File", typeof(string));
+                    dt.Columns.Add("English", typeof(string));
+
+                    foreach (var item in list)
+                    {
+                        dt.Rows.Add(item.File, item.English);
+                    }
+
+                    exportExcell(dt, "export.xlsx");
+                    progress.Report(100);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            });
+
+            return true;
+        }
+
+        private void exportExcell(DataTable dt, string fileName)
+        {
+            FileInfo file = new FileInfo(Path.Combine(txtExportFolder.Text, fileName));
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = null;
+                var count = package.Workbook.Worksheets.Count(n => n.Name == "Accounts");
+                if (count == 0)
+                {
+                    worksheet = package.Workbook.Worksheets.Add("Accounts");
+                }
+                else
+                {
+                    worksheet = package.Workbook.Worksheets.FirstOrDefault(n => n.Name == "Accounts");
+                }
+                int totalRows = dt.Rows.Count;
+                worksheet.Cells[1, 1].Value = "File";
+                worksheet.Cells[1, 2].Value = "English";
+                int i = 0;
+                for (int row = 2; row <= totalRows + 1; row++)
+                {
+                    worksheet.Cells[row, 1].Value = dt.Rows[i]["File"];
+                    worksheet.Cells[row, 2].Value = dt.Rows[i]["English"];
+                    i++;
+                }
+
+                package.Save();
+
+            }
+        }
+
+        private string GetPathFile(string path)
+        {
+            return Path.GetExtension(path);
+        }
+
+        private List<string> GetStringFromXML(string path)
+        {
+            List<string> listStr = new List<string>();
+            //Create the XmlDocument.
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            //Display all the book titles.
+            XmlNodeList elemList = doc.GetElementsByTagName("arwctl:Phrase");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                listStr.Add(elemList[i].InnerXml);
+            }
+
+            return listStr;
+        }
+
+        private List<string> GetStringFromASP(string path)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            List<string> listStr = new List<string>();
+            string readText = File.ReadAllText(path);
+
+            if (string.IsNullOrWhiteSpace(readText)) return listStr;
+
+            doc.LoadHtml(readText);
+
+            foreach (HtmlAgilityPack.HtmlTextNode node in doc.DocumentNode.SelectNodes("//text()"))
+            {
+                string temp = node.Text;
+                temp = Regex.Replace(temp, @"\s", string.Empty);
+                if (!string.IsNullOrEmpty(temp))
+                    listStr.Add(temp);
+            }
+            return listStr;
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = 0;
+                if (string.IsNullOrWhiteSpace(txtFolder.Text) || string.IsNullOrWhiteSpace(txtExportFolder.Text))
+                {
+                    MessageBox.Show("Please choice Folder Before");
+                    return;
+                }
+
+                if (!rdExcelToXML.Checked && !rdXmlToExcel.Checked)
+                {
+                    MessageBox.Show("Please choice type of convert Before");
+                    return;
+                }
+
+
+                if (rdXmlToExcel.Checked) await convertXMLToExcel();
+
+                if (rdExcelToXML.Checked) await convertExcellToXML();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+        }
+
+        private void btnChoiceImportFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Please select folder";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtFolder.Text = fbd.SelectedPath;
+        }
+
+        private void btnChoiceFolderExport_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Please select folder";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtExportFolder.Text = fbd.SelectedPath;
+        }
+    }
+
+    public class ExportFile
+    {
+        public string File { get; set; }
+        public string English { get; set; }
+        public string NewLang { get; set; }
+        public int? Length { get; set; }
+    }
+}
